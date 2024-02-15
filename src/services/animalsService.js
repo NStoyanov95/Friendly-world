464a811c86a2d7abd9fba6c74851de871385d041
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-15 17:25:35 +0200

    Add get all animals and get last 3 animals

--- src/services/animalsService.js
+++ src/services/animalsService.js
@@ -1,3 +1,7 @@
 const Animal = require('../models/Animal');
 
+exports.getAll = () => Animal.find();
+
+exports.getLastThree = () => Animal.find().sort({createdAt: -1}).limit(3);
+
 exports.create = (animalData) => Animal.create(animalData);
